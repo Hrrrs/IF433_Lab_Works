4e65335d6748aa122412fa984b7031fa751c662a
Hrrrs <bopakku@gmail.com>
2026-02-06 16:21:09 +0700

    week01: refaactor to use val and string templates

--- src/oop_00000114587_aufadanam/week01/GradingSystem.kt
+++ src/oop_00000114587_aufadanam/week01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_00000114587_aufadanam.week01
 
 fun main (){
-    var name: String = "johny johny"
-    var score: Int = 80
+    val name: String = "johny johny"
+    val score: Int = 80
 
     // concatenation style
-    println("Nama: " + name + " score is " + score)
+    println("Nama $name, Nilai: $score")
 }
