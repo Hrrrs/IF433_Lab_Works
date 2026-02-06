aa7e23f18c2a6f83e147a7eeb35b478037b0993c
Hrrrs <bopakku@gmail.com>
2026-02-06 16:16:01 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000114587_aufadanam/week01/GameStore.kt
@@ -0,0 +1,2 @@
+package oop_00000114587_aufadanam.week01
+
---
+++ src/oop_00000114587_aufadanam/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000114587_aufadanam.week01
+
+fun main (){
+    var name: String = "johny johny"
+    var score: Int = 80
+
+    // concatenation style
+    println("Nama: " + name + " score is " + score)
+}
