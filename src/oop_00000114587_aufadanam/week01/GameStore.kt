881f20a9090ada9872ff1bd6821d58c09d31727d
Hrrrs <bopakku@gmail.com>
2026-02-12 00:48:31 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000114587_aufadanam/week01/GameStore.kt
+++ src/oop_00000114587_aufadanam/week01/GameStore.kt
@@ -5,18 +5,19 @@ import kotlin.math.roundToInt
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 599000
-
+    val userNote: String? = null
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
 
-    printReceipt(title = gameTitle, total = finalPrice)
+    printReceipt(title = gameTitle, total = finalPrice, note = userNote?: "tidak ada catatan")
 }
 
 fun calculateDiscount (price: Int): Int= if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-fun printReceipt(title: String, total: Int) {
+fun printReceipt(title: String, total: Int, note: String) {
     println("------- Struk SteamKW -------")
     println("Game           : $title")
     println("harga discount : Rp $total")
+    println("catatan: $note")
     println("-----------------------------")
 }
