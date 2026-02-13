a5acb4865173c70da6d3a308f892c6fec7333342
Hrrrs <bopakku@gmail.com>
2026-02-13 20:48:40 +0700

    week02: (task 1) implement library input system

--- src/oop_00000114587_aufadanam/week02/Loan.kt
+++ src/oop_00000114587_aufadanam/week02/Loan.kt
@@ -1,5 +1,7 @@
 package oop_00000114587_aufadanam.week02
 
+import java.util.Scanner
+
 class Loan(
     val bookTitle: String,
     val borrower: String,
@@ -14,4 +16,33 @@ class Loan(
             0
         }
     }
+}
+
+fun main() {
+    val input = Scanner(System.`in`)
+
+    // 1. Input
+    print("Masukkan Judul Buku: ")
+    val bookTitle = input.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = input.nextLine()
+
+    print("Masukkan Lama Pinjam (hari): ")
+    var loanDuration = input.nextInt()
+
+    // 2. Validasi: Jika minus, ubah menjadi 1
+    if (loanDuration < 0) {
+        loanDuration = 1
+    }
+
+    // 3. Membuat objek
+    val myLoan = Loan(bookTitle, borrower, loanDuration)
+
+    // print detail peminjaman dan total denda
+    println("\n--- DATA PEMINJAMAN ---")
+    println("Buku    : ${myLoan.bookTitle}")
+    println("Peminjam: ${myLoan.borrower}")
+    println("Durasi  : ${myLoan.loanDuration} hari")
+    println("Denda   : Rp ${myLoan.calculateFine()}")
 }
