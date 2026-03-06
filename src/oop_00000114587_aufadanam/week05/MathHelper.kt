86a160b60c3039a387d695086b21dc85d887ae7a
Hrrrs <bopakku@gmail.com>
2026-03-06 14:49:07 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000114587_aufadanam/week05/MathHelper.kt
@@ -0,0 +1,19 @@
+package oop_00000114587_aufadanam.week05
+
+class MathHelper {
+
+    // 1. Menghitung luas persegi
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    // 2. Menghitung luas persegi panjang
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    // 3. Menghitung luas lingkaran
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
