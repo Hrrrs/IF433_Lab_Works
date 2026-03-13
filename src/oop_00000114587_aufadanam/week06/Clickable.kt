2db2ec7b14c7c7f892c6bd71a249ef045e0013b3
Hrrrs <bopakku@gmail.com>
2026-03-13 14:05:03 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000114587_aufadanam/week06/Clickable.kt
+++ src/oop_00000114587_aufadanam/week06/Clickable.kt
@@ -1,6 +1,6 @@
 package oop_00000114587_aufadanam.week06
 
 interface Clickable {
-    val name: String = "Tombol Rahasia"
+    val name: String // Abstract property, tidak ada nilai default
     fun click()
 }
