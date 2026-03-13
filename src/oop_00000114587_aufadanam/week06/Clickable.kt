44a254b1503bb3cf15675f7d75026eae9f97acc9
Hrrrs <bopakku@gmail.com>
2026-03-13 14:02:16 +0700

    week06: (trial) create Clickable interface with backing field error

---
+++ src/oop_00000114587_aufadanam/week06/Clickable.kt
@@ -0,0 +1,9 @@
+package oop_00000114587_aufadanam.week06
+
+class Clickable {
+    // ERROR: Property in an interface cannot have a backing field
+    val name: String = "Tombol Rahasia"
+
+    // Function without body (implicitly Abstract)'
+    fun click()
+}
