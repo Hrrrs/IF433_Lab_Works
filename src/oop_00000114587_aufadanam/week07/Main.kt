0d4808007cd117b5e632ac1f9ae341388913050d
Hrrrs <bopakku@gmail.com>
2026-03-27 21:36:50 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000114587_aufadanam/week07/Main.kt
+++ src/oop_00000114587_aufadanam/week07/Main.kt
@@ -1,35 +1,37 @@
 package oop_00000114587_aufadanam.week07
 
 fun main() {
-    println("=== TEST SINGLETON ===")
-    println("status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
-
-    println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1) // Akan mencetak memori hash
-    println("sama? ${reg1 == reg2}") // False
-
-    println("\n=== TEST DATA CLASS ===")
-    val data1 = DataUser ("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-    println(data1)
-    println("sama? ${data1 == data2}")
-
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-
-    val (userName, age) = data1 // destructuring declaration
-    println("destructured: $userName berumur $userName")
-
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    val uiMessage = when(response) {
-        is ApiResponse.Loading -> "Tampilkan Spinner"
-        is ApiResponse.Success -> "Tampilkan : ${response.data}"
-        is ApiResponse.Error -> "Tampilkan : ${response.message}"
-    }
-    println(uiMessage)
+//    println("=== TEST SINGLETON ===")
+//    println("status: ${DatabaseManager.connectionStatus}")
+//    DatabaseManager.connect()
+//
+//    println("\n=== TEST REGULAR CLASS ===")
+//    val reg1 = RegularUser("Alice", 22)
+//    val reg2 = RegularUser("Alice", 22)
+//    println(reg1) // Akan mencetak memori hash
+//    println("sama? ${reg1 == reg2}") // False
+//
+//    println("\n=== TEST DATA CLASS ===")
+//    val data1 = DataUser ("Alice", 22)
+//    val data2 = DataUser("Alice", 22)
+//    println(data1)
+//    println("sama? ${data1 == data2}")
+//
+//    val data3 = data1.copy(age = 23)
+//    println("Hasil Copy: $data3")
+//
+//    val (userName, age) = data1 // destructuring declaration
+//    println("destructured: $userName berumur $userName")
+//
+//    println("\n=== TEST SEALED CLASS ===")
+//    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+//
+//    val uiMessage = when(response) {
+//        is ApiResponse.Loading -> "Tampilkan Spinner"
+//        is ApiResponse.Success -> "Tampilkan : ${response.data}"
+//        is ApiResponse.Error -> "Tampilkan : ${response.message}"
+//    }
+//    println(uiMessage)
+    GameManager.startGame()
+    GameManager.startGame()
 }
