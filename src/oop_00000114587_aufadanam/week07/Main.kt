e2ff86f5e60daf74aa82c56f9513395108277fd9
Hrrrs <bopakku@gmail.com>
2026-03-27 15:51:44 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_00000114587_aufadanam/week07/Main.kt
+++ src/oop_00000114587_aufadanam/week07/Main.kt
@@ -4,4 +4,10 @@ fun main() {
     println("=== TEST SINGLETON ===")
     println("status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1) // Akan mencetak memori hash
+    println("sama? ${reg1 == reg2}") // False
 }
