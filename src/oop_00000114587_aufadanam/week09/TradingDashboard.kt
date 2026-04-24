b100284f2a296586d07952651b692bde557eb832
Hrrrs <bopakku@gmail.com>
2026-04-24 15:51:08 +0700

    week09: (task)  print top performers using forEach

--- src/oop_00000114587_aufadanam/week09/TradingDashboard.kt
+++ src/oop_00000114587_aufadanam/week09/TradingDashboard.kt
@@ -27,10 +27,10 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
-//
-//    println("=== CRYPTO TRADING DASHBOARD ===")
-//    topPerformersString.forEach { println(it) }
-//
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
+
 //    worstPerformersString.forEach { println(it) }
 //
 //    println(uniquePairs)
