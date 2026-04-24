1de3c50fecbba6dbf5d6c65212b394fd7b78d735
Hrrrs <bopakku@gmail.com>
2026-04-24 15:44:49 +0700

    week09: (task) populate mock trade history list

--- src/oop_00000114587_aufadanam/week09/TradingDashboard.kt
+++ src/oop_00000114587_aufadanam/week09/TradingDashboard.kt
@@ -10,28 +10,28 @@ fun main() {
         TradeLog("ETHUSDT", "LONG", 20, -15.0, "CLOSED")
     )
 
-    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
-
-    val winningTrades = closedTrades.filter { it.roe > 0 }
-
-    val losingTrades = closedTrades.filter { it.roe <= 0 }
-
-    val topPerformersString = winningTrades
-        .sortedByDescending { it.roe }
-        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
-
-    val worstPerformersString = losingTrades
-        .sortedBy { it.roe }
-        .map { "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
-
-    val uniquePairs = tradeHistory
-        .map { it.pair }
-        .toSet()
-
-    println("=== CRYPTO TRADING DASHBOARD ===")
-    topPerformersString.forEach { println(it) }
-
-    worstPerformersString.forEach { println(it) }
-
-    println(uniquePairs)
+//    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+//
+//    val winningTrades = closedTrades.filter { it.roe > 0 }
+//
+//    val losingTrades = closedTrades.filter { it.roe <= 0 }
+//
+//    val topPerformersString = winningTrades
+//        .sortedByDescending { it.roe }
+//        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+//
+//    val worstPerformersString = losingTrades
+//        .sortedBy { it.roe }
+//        .map { "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
+//
+//    val uniquePairs = tradeHistory
+//        .map { it.pair }
+//        .toSet()
+//
+//    println("=== CRYPTO TRADING DASHBOARD ===")
+//    topPerformersString.forEach { println(it) }
+//
+//    worstPerformersString.forEach { println(it) }
+//
+//    println(uniquePairs)
 }
