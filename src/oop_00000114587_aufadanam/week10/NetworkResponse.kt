555ceb07e26cb4a211d394c338d41261d686dd88
Hrrrs <bopakku@gmail.com>
2026-05-08 09:22:43 +0700

    week10 : (task) implement search logic in repository

--- src/oop_00000114587_aufadanam/week10/NetworkResponse.kt
+++ src/oop_00000114587_aufadanam/week10/NetworkResponse.kt
@@ -4,3 +4,7 @@ data class ApiResponse<T>(
     val status: String,
     val data: T
 )
+
+fun <T : NamedEntity> WalletRepository<T>.searchByName(query: String): List<T> {
+    return getAll().filter { it.name.contains(query, ignoreCase = true) }
+}
